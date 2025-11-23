b162ab45c5852af7fee71fc21bb4b12d30d6c30d
ynw12 <yeonwoo845@gmail.com>
2025-11-24 05:25:44 +0900

    Update Order_Ready.java
    주문 완료 프로토콜 수정

--- Order_Ready.java
+++ Order_Ready.java
@@ -14,13 +14,13 @@ public class Order_Ready {
                 // 대기열에서 손님 제거
                 queueLogic.subClient(session);
 
-                // 손님한테 ORDER_READY 보냄 자세한 출력문은 완성되고 수정 ㄱㄱ
-                broadcaster.sendTo(session,
-                        "ORDER_READY " + session.getno());
+                //서버 -> 클라 ORDER_READY 1101 아메리카노 (프로토콜 양식 : ORDER_READY 주문번호 주문메뉴명)
+                //이부분 파싱하시면 됩니다! 주문번호랑 주문메뉴명 같이 보내니까 UI에서 주문완료 띄울 때 1101님 아메리카노 준비됐습니다
+                //UI에서 주문완료 띄울 때 1101님 아메리카노 준비됐습니다 이런식으로 주문 메뉴까지 화면에 나오게 가도 될 것 같아요
+                broadcaster.sendTo(session, "ORDER_READY " + session.getNo() + session.getName());
 
                 // 다른 손님들에게도 대기열 보여줌 -> 손님 빠질 때마다 확인 ㄱㄴ임 
-                broadcaster.broadcast(queueLogic.getClientList(),
-                		 "대기열 변경 알림");
+                broadcaster.broadcast(queueLogic.getClientList(), "대기열 변경 알림");
 
             } catch (Exception e) {
                 e.printStackTrace();
@@ -28,3 +28,4 @@ public class Order_Ready {
         }, 1, TimeUnit.MINUTES); // 일단은 1분 설정 하고 나중에 테스트 해보다 수정 ㄱㄱ
     }
 }
+
