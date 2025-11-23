d63db740dc17b31c39da515b8bcbc156eb1409aa
ynw12 <yeonwoo845@gmail.com>
2025-11-24 05:17:47 +0900

    Update OrderDAOimpl.java

--- OrderDAOimpl.java
+++ OrderDAOimpl.java
@@ -16,17 +16,16 @@ public class OrderDAOimpl {
     }
     public void CreateOrder(OrderDTO order) {
         String sql = "INSERT INTO ordermanagement (name) VALUES (?)";
-        try (Connection conn = connector.getConnection();
-             PreparedStatement pstmt =
-                 conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+         try (Connection conn = connector.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+        	//손님의 주문 들어옴 -> 손님에게 주문번호 1 부여 -> 다음손님부터 번호 : 2,3,4,5...
         	pstmt.setString(1, order.getName());
+        	pstmt.setString(2, order.getName());
             pstmt.executeUpdate();
-            //손님 주문 들어올 때 주문번호:no는 1부터 자동 생성
             try(ResultSet generatedKeys = pstmt.getGeneratedKeys()){
             	if(generatedKeys.next()) {
             		int no = generatedKeys.getInt(1);
             		order.setNo(no);
-            		System.out.println(no + "");
             	}
             }
         } catch (SQLException e) {
