2e8f625190f8144b6c1d0d31b83e9e4b7b646d56
ynw12 <yeonwoo845@gmail.com>
2025-11-24 19:49:53 +0900

    Add DoneOrder

---
+++ DoneOrderDTO.java
@@ -0,0 +1,18 @@
+package Server_v1;
+
+public class DoneOrderDTO {
+	private int doneNo;
+	private String doneName;
+	public int getDoneNo() {
+		return doneNo;
+	}
+	public void setDoneNo(int doneNo) {
+		this.doneNo = doneNo;
+	}
+	public String getDoneName() {
+		return doneName;
+	}
+	public void setDoneName(String donename) {
+		this.doneName = doneName;
+	}
+}
