4826cc599401500dfa2a5b92f7953094ff2fe53c
ynw12 <yeonwoo845@gmail.com>
2025-11-27 01:37:35 +0900

    Update DoneOrderService.java

--- DoneOrderService.java
+++ DoneOrderService.java
@@ -4,12 +4,17 @@ import java.util.List;
 
 public class DoneOrderService {
 	private final DatabaseConnector connector; 
-	public DoneOrderService(DatabaseConnector connector) {
+	private final OrderDAOimpl orderDAO;
+	/*public DoneOrderService(DatabaseConnector connector) {
 		this.connector = connector;
+	}*/
+	public DoneOrderService(OrderDAOimpl orderDAO,DoneOrderDAO doneOrderDAO){
+		this.orderDAO = orderDAO;
+		this.doneOrderDAO = doneOrderDAO;
 	}
 	
 	// 기능1. 완료된 주문내역 전체 출력
-	public void showDoneOrder(DoneOrderDAO doneOrderDAO) {
+	public void showDoneOrder() {
 		List<DoneOrderDTO> doneOrderList = doneOrderDAO.getAllDoneOrders();
 		for(DoneOrderDTO doDTO : doneOrderList) {
 			System.out.println("======완료된 주문 내역======");
@@ -19,9 +24,10 @@ public class DoneOrderService {
 
 	// 기능2. pickup 처리 
 	// OrderDAO로 key값(no)으로 원하는 주문내역 읽어오기 -> DoneOrderDAO로 가져온 주문내역을 doneorder 테이블에 삽입하기 
-	public void pickUpDoneOrder() {
-		
+	public void pickUpDoneOrder(int no) {
+		orderDAO.moveOrderToDone(no)
 		DoneOrderDAO.insertDoneOrder();
 		
 	}
+
 }
