ca8f2e8ddfa8338b37dddce1402ed6d9f3165b2d
quizelf <quizelf2@gmail.com>
2025-11-27 02:22:30 +0900

    Implement order deletion after pickup
    메소드 추가 : 
    - pickup 처리
    - pickup 후 삭제(픽업 완료된 주문 처리)

--- DoneOrderService.java
+++ DoneOrderService.java
@@ -24,13 +24,19 @@ public class DoneOrderService {
 	}
 
 	// 기능2. pickup 처리 
-	// OrderDAO로 key값(no)으로 원하는 주문내역 읽어오기 -> DoneOrderDAO로 가져온 주문내역을 doneorder 테이블에 삽입하기 
+	// OrderDAO로 key값(no)으로 원하는 주문내역 읽어오기 -> DoneOrderDAO로 가져온 주문내역을 doneorder 테이블에 삽입하기 (moveOrderToDone(no))
 	public void pickUpDoneOrder(int no) {
 		orderDAO.moveOrderToDone(no);
-		DoneOrderDAO.insertDoneOrder();
-		
+		System.out.println("메뉴가 완료되었습니다! 픽업대에서 음료를 픽업해주세요"); 
 	}
-
+	
+	// 기능 3. pickup 후 삭제(픽업 완료된 주문 처리) 
+	// 관리자 UI에서 픽업완료 신호를 주면 -> key값(no)으로 원하는 주문내역을 doneorder 테이블에서 삭제 
+	public void DonePickUp(int no) {
+		doneOrderDAO.deleteDoneOrder(no);	
+		System.out.println("손님이 음료를 픽업해갔습니다. 주문 끝!"); 
+	}
 }
+
 
 
