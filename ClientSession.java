cd6269ae9ff72320285c7dc3f54897a6f1603db5
ynw12 <yeonwoo845@gmail.com>
2025-11-27 03:15:11 +0900

    Update ClientSession.java

--- ClientSession.java
+++ ClientSession.java
@@ -4,13 +4,19 @@ import java.io.PrintWriter;
 
 public class ClientSession {
     private final PrintWriter out;   // 이 손님에게 메시지 보낼 통로
-    private int no;    //주문번호
-    private String name;// 주문한 메뉴명
-    //기존 Client -> ClientSession 로 변경
+    private int no; //주문번호
+    private String name;//주문한 메뉴명
+    private String status;
+    public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	//기존 Client -> ClientSession 로 변경
     public ClientSession(PrintWriter out) {
         this.out = out;
-         }
-
+    }    
     public PrintWriter getOut() { 
     	return out; 
     }
@@ -20,7 +26,6 @@ public class ClientSession {
     public int getNo() { 
     	return no; 
     }
-    //주문메뉴명 메소드 변경
     public void setName(String name) {
     	this.name = name;
     }
@@ -28,5 +33,4 @@ public class ClientSession {
     	return name;
     }
    
-
 }
