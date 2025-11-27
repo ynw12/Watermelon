cf95485d9b56112e8f717ecc87d0b98e331a5c79
ynw12 <yeonwoo845@gmail.com>
2025-11-27 16:19:56 +0900

    Add files of StaffSessions
    스태프세션(pickup)입력해줄 클래스 추가하고 clientSession이랑 같이 공통기능은 인터페이스로 묶어뒀습니다

---
+++ Session.java
@@ -0,0 +1,7 @@
+package Server_v1;
+
+import java.io.PrintWriter;
+
+public interface Session {
+	PrintWriter getOut();
+}
---
+++ StaffSession.java
@@ -0,0 +1,16 @@
+package Server_v1;
+
+import java.io.PrintWriter;
+
+public class StaffSession implements Session{
+	private final PrintWriter out;
+	//@Override
+	public StaffSession (PrintWriter out) {
+		this.out = out;
+	}
+	public PrintWriter getOut() {
+		// TODO Auto-generated method stub
+		return out;
+	}
+	
+}
