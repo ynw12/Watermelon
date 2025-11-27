98549f212cdb6a236cc6ca5a40fffa1cc1d2684b
ynw12 <yeonwoo845@gmail.com>
2025-11-27 16:16:28 +0900

    FIx ServerWorker.java
    StaffSession 연동, 메뉴 서비스 호출 연동

--- ServerWorker.java
+++ ServerWorker.java
@@ -10,16 +10,22 @@ import java.net.Socket;
 public class ServerWorker extends Thread {
 
     private final Socket socket;
+    
     private final OrderService orderService;
+    private final DoneOrderService doneorderService;
 
     private BufferedReader in;
     private PrintWriter out;
 
-    private ClientSession session;
+    // 손님용 세션
+    private ClientSession clientSession;
+    // 직원용 세션
+    private StaffSession staffSession;
 
-    public ServerWorker(Socket socket, OrderService orderService) throws IOException {
+    public ServerWorker(Socket socket, OrderService orderService, DoneOrderService doneorderService) throws IOException {
         this.socket = socket;
         this.orderService = orderService;
+        this.doneorderService = doneorderService;
 
         this.in  = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         this.out = new PrintWriter(socket.getOutputStream(), true);
@@ -34,19 +40,61 @@ public class ServerWorker extends Thread {
             while ((msg = in.readLine()) != null) {
                 System.out.println("[SERVER] 수신: " + msg);
 
-                // 1) 주문 생성: NEW_ORDER
+                String[] parts = msg.split(" ");
+                String cmd = parts[0];
+
+                //직원 접속
+                if (cmd.equals("STAFF_HELLO")) {
+                    staffSession = new StaffSession(out);
+                    out.println("STAFF_LOGIN");
+                    System.out.println("[SERVER] Staff 세션 생성");
+                    continue;
+                }
+                else if(cmd.equals("CLIENT_HELLO")){
+                	clientSession = new ClientSession(out);
+                	out.println("Client_LOGIN");
+                	System.out.println("[SERVER] Client 세션 생성");
+                	MenuService.showAllMenus();
+                    continue;
+                }
+
+                //관리자 명령 처리 -> 프로토콜 확인
+                if (staffSession != null) {
+                    switch (cmd) {
+                        case "STAFF_GET_DONE":
+                            // 제조 완료(DONE) 상태이면서 아직 픽업 안 된 손님 목록 보내기
+                            doneorderService.showDoneOrder(staffSession);
+                            break;
+
+                        case "PICKUP":
+                            // 
+                            doneorderService.handleStaffPickup(msg, clientSession);
+                            break;
+
+                        case "DISCONNECT":
+                            System.out.println("[SERVER] STAFF DISCONNECT 요청");
+                            return; // run() 종료 → finally에서 소켓 정리
+
+                        default:
+                            out.println("ERROR_STAFF");
+                            break;
+                    }
+                    continue;
+                }
+
+                //고객
+                // 1) 주문 생성: NEW_ORDER 주문메뉴명 (클라 -> 서버 프로토콜 양식)
                 if (msg.startsWith("NEW_ORDER")) {
-                    session = orderService.handleNewOrder(msg, session, out);
+                    orderService.handleNewOrder(msg, clientSession, out);
                 }
-                // 2) 상태 조회: GET_STATUS
+                // 2) 상태 조회: GET_STATUS (클라 -> 서버 프로토콜 양식)
                 else if (msg.equals("GET_STATUS")) {
-                    orderService.handleGetStatus(session);
+                    orderService.handleGetStatus(clientSession);
                 }
-                /* 3) 앱 종료: DISCONNECT (얘는 왜 있냐면 클라이언트 소켓 종료때문에 만들어뒀어요
-                 * 클라 소켓, 서버 소켓 모두 종료를 시켜야 하는데 이부분을 어떻게 해야할지 모르겠어서 
-                 * 임시방편으로 지피티 도움 받아서 만들었습니다 그래서 UI에서 추가할 부분은 
-                 * DISCONNECT 요청할 버튼만 있으면 될 것 같은데 저도 공부 더 해보고 어떤 거 더 추가해야하는지 
-                 * 알아볼게요 일단은 발표할 때 여기 구현은 제외하고 발표하는게 좋을 것 같아요
+
+                /* 3) 앱 종료: DISCONNECT
+                 * 클라 소켓, 서버 소켓 모두 종료를 시켜야 하는데
+                 * 임시방편으로 DISCONNECT로 연결 종료
                  */
                 else if (msg.equals("DISCONNECT")) {
                     System.out.println("[SERVER] 클라이언트 DISCONNECT 요청");
@@ -54,7 +102,7 @@ public class ServerWorker extends Thread {
                 }
                 // 4) 그 외 명령
                 else {
-                    out.println("ERROR UNKNOWN_COMMAND");
+                    out.println("ERROR_Client");
                 }
             }
         } catch (IOException e) {
@@ -70,5 +118,4 @@ public class ServerWorker extends Thread {
             System.out.println("[SERVER] 클라이언트 연결 종료: " + socket.getRemoteSocketAddress());
         }
     }
-
 }
